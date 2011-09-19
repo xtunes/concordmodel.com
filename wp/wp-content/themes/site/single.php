368bbc047f37576d5bb7cf90304afb0ad14b0858
Eric <eric@xtunes.cc>
2011-09-19 16:30:57 +0800

    添加面包屑

--- wp/wp-content/themes/site/single.php
+++ wp/wp-content/themes/site/single.php
@@ -9,6 +9,14 @@
 
 get_header(); ?>
 <div id="content" role="main">
+	<div class="breadcrumbs">
+	<?php
+if(function_exists('bcn_display'))
+{
+    bcn_display();
+}
+?>
+</div>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 					<?php the_content(); ?>
 		<?php endwhile; // end of the loop. ?>
