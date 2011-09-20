ec473a599ff7d3b06f46961d1083e2e4c8e8d505
Eric <eric@xtunes.cc>
2011-09-20 13:31:54 +0800

    恫森资讯

--- wp/wp-content/themes/site/category.php
+++ wp/wp-content/themes/site/category.php
@@ -12,21 +12,12 @@ get_header(); ?>
 		<div id="container">
 			<div id="content" role="main">
 
-				<h1 class="page-title"><?php
-					printf( __( 'Category Archives: %s', 'twentyten' ), '<span>' . single_cat_title( '', false ) . '</span>' );
+				<h1 class="page-title"><?php printf( '<span>' . single_cat_title( '', false ) . '</span>' );
 				?></h1>
-				<?php
-					$category_description = category_description();
-					if ( ! empty( $category_description ) )
-						echo '<div class="archive-meta">' . $category_description . '</div>';
-
-				/* Run the loop for the category page to output the posts.
-				 * If you want to overload this in a child theme then include a file
-				 * called loop-category.php and that will be used instead.
-				 */
-				get_template_part( 'loop', 'category' );
-				?>
-
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<span><?php the_time('Y年m月j日') ?></span><a href="<?php the_permalink() ?>"><h4><?php the_title(); ?></h4></a>
+				   <?php endwhile; endif; ?>
+				  				 <?php if(function_exists('wp_pagenavi')) { wp_pagenavi(); } ?>
 			</div><!-- #content -->
 		</div><!-- #container -->
 
