7dd3af4e4ca6f541fcfbdf5d3cfb0fe7047562d2
Eric <eric@xtunes.cc>
2011-09-20 13:41:54 +0800

    资讯样式

--- wp/wp-content/themes/site/category.php
+++ wp/wp-content/themes/site/category.php
@@ -12,12 +12,14 @@ get_header(); ?>
 		<div id="container">
 			<div id="content" role="main">
 
-				<h1 class="page-title"><?php printf( '<span>' . single_cat_title( '', false ) . '</span>' );
+				<h1 style="font-size:18px;margin:20px 0 0 50px"><?php printf( '<span>' . single_cat_title( '', false ) . '</span>' );
 				?></h1>
+				<div style="margin:40px auto;width:915px;">
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<span><?php the_time('Y年m月j日') ?></span><a href="<?php the_permalink() ?>"><h4><?php the_title(); ?></h4></a>
+<a href="<?php the_permalink() ?>"><span><?php the_time('Y年m月j日') ?></span><h4><?php the_title(); ?></h4></a>
 				   <?php endwhile; endif; ?>
 				  				 <?php if(function_exists('wp_pagenavi')) { wp_pagenavi(); } ?>
+				 </div>
 			</div><!-- #content -->
 		</div><!-- #container -->
 
